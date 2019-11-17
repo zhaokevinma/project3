924da937f81cdae3934721efd57bd14d9c7c282d
Kevin Malone <zhaokevinma@gmail.com>
2019-11-17 16:19:36 -0500

    Able to retrieve patient data from db from api route

---
+++ models/index.js
@@ -0,0 +1,4 @@
+// ------ Export Patient
+module.exports = {
+    Patient: require("./patient")
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 // ------ Import Router from Express
 const router = require("express").Router();
 
+// ------ db
+const db = require("../models");
+
 // ------ GET request to reach /test
 router.get("/test", (req, res) => {
     res.send("Test route");
+})
+
+// ------ GET request to fetch patients
+router.get("/api/patients", (req, res) => {
+    db.Patient
+        .find({})
+        .then(data => res.json(data))
+        .catch(err => res.status(422).json(err));
 })
 
 // ------ Export router
