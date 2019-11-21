e9df1bac0ed8a49a4aa6ea65f9185c671f72b22e
Kevin Malone <zhaokevinma@gmail.com>
2019-11-21 16:58:32 -0500

    2nd attempt to fix heroku

---
+++ client/build/precache-manifest.e1aaf062df80d10c1a09adf734323549.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40973a5830b41841871717cd2895f1a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2383ca49224ad0a8e507",
+    "url": "/static/css/main.0fba2964.chunk.css"
+  },
+  {
+    "revision": "af589653b5b613c7f344",
+    "url": "/static/js/2.bb4a70e7.chunk.js"
+  },
+  {
+    "revision": "2383ca49224ad0a8e507",
+    "url": "/static/js/main.be282036.chunk.js"
+  },
+  {
+    "revision": "51d2be9524d992a37e32",
+    "url": "/static/js/runtime-main.c51f125f.js"
+  }
+]);
