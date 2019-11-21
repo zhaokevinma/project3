da7f93b2f56fd350cddf6ca44c0a86fbe6da8622
Kevin Malone <zhaokevinma@gmail.com>
2019-11-20 22:10:38 -0500

    Some front end styling change

--- client/src/components/Folder/index.js
+++ client/src/components/Folder/index.js
@@ -1,6 +1,8 @@
 // ------ Dependencies
 import React from "react";
 import { Accordion, Card, Button } from 'react-bootstrap'
+import { Row, Col} from "../Grid";
+import "./style.css";
 
 // ------ Folder
 const Folder = props => {
@@ -13,7 +15,14 @@ const Folder = props => {
                         <Card>
                             <Card.Header>
                             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                <i className="fas fa-folder-plus fa-2x"></i>
+                                <Row>
+                                    <Col size="3">
+                                        <i className="fas fa-folder-plus fa-2x"></i>
+                                    </Col>
+                                    <Col size="6">
+                                        <h4>Create folder</h4>
+                                    </Col>
+                                </Row>
                             </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
@@ -26,7 +35,8 @@ const Folder = props => {
                                     value={props.newFolder}
                                     onChange={props.handleNewFolder}
                                 />
-                                <Button 
+                                <Button block
+                                    className="createFolder"
                                     type="submit"
                                     onClick={props.handleCreateFolder}
                                 >
@@ -39,7 +49,14 @@ const Folder = props => {
                     {props.folders.map(folder => {
                         return (
                             <li className="list-group-item" key={folder._id}>
-                                <h5 id={folder._id} onClick={props.folderFilter}>{folder.folderName} {folder.patients.length}</h5>
+                                <Row>
+                                    <Col size="10">
+                                        <h5 id={folder._id} onClick={props.folderFilter}>{folder.folderName}</h5>
+                                    </Col> 
+                                    <Col size="2">
+                                        <h5>{folder.patients.length}</h5>
+                                    </Col>
+                                </Row>
                             </li>
                         );
                     })}
