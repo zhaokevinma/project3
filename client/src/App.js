91ef0d175eda65b28274ce1d8316dc6fee2d2e91
Kevin Malone <zhaokevinma@gmail.com>
2019-12-01 00:36:46 -0500

    not footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
 import WorklistPage from "./pages/Worklist";
 import NoMatch from "./pages/NoMatch";
 
@@ -17,7 +16,6 @@ function App() {
           <Route exact path="/" component={WorklistPage} />
           <Route component={NoMatch} />
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
